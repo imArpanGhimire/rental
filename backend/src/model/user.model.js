7c24d06f009bd87f31045315a47af678add97d9c
imArpanGhimire <arpancalma@gmail.com>
2026-06-19 22:56:57 +0545

    Add user model schema for MongoDB

---
+++ backend/src/model/user.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const userschema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 20
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+
+    }
+})
