e4c618e73478f6174da59ebcc0c0d939712703f3
imArpanGhimire <arpancalma@gmail.com>
2026-06-21 15:21:04 +0545

    Add cookie parser middleware for handling cookies

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,17 @@
 const express = require("express")
 const app = express()
+const cookieParser = require("cookie-parser")
 
 app.use(express.json())
+app.use(cookieParser())
 
 
 const authroutes = require("./routes/auth.routes")
+const propertyroutes = require("./routes/property.routes")
 
 
 app.use("/api/auth", authroutes)
+app.use("/api/properties", propertyroutes)
 
 
 module.exports = app
