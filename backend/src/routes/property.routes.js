9801f0ba88597aef7b0f97efe226455621bb63d5
imArpanGhimire <arpancalma@gmail.com>
2026-06-22 00:03:28 +0545

    Add delete and view my listings routes with appropriate middleware

--- backend/src/routes/property.routes.js
+++ backend/src/routes/property.routes.js
@@ -7,11 +7,15 @@ const ownershipMiddleware = require("../middleware/owner.middleware")
 const verifyPropertyMiddleware = require("../middleware/verifyproperty.middleware")
 
 const propertycontroller = require("../controller/property.controller")
+const authmiddleware = require("../middleware/auth.middleware")
+const verifypropertyowner = require("../middleware/verifyproperty.middleware")
 
 
 router.post("/add-property", authMiddleware, ownershipMiddleware, propertycontroller.createproperty)
 router.get("/get-all-properties", propertycontroller.getallproperties)
 router.get("/get-property/:id", propertycontroller.getoneproperty)
 router.put("/update-property/:id", authMiddleware, ownershipMiddleware, verifyPropertyMiddleware, propertycontroller.updateproperty)
+router.delete("/delete-property/:id", authMiddleware, ownershipMiddleware, verifyPropertyMiddleware, propertycontroller.deleteproperty)
+router.get("/view-my-listings", authmiddleware, ownershipMiddleware, verifypropertyowner, propertycontroller.getmyproperties)
 
 module.exports = router
