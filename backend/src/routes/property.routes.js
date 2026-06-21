18308694067315a46e02fcdfbaf0b6655f879cd2
imArpanGhimire <arpancalma@gmail.com>
2026-06-21 17:31:29 +0545

    Remove auth middleware from get-all-properties route

--- backend/src/routes/property.routes.js
+++ backend/src/routes/property.routes.js
@@ -6,6 +6,7 @@ const propertycontroller = require("../controller/property.controller")
 const authmiddleware = require("../middleware/auth.middleware")
 
 router.post("/add-property", authmiddleware, propertycontroller.createproperty)
-router.get("/get-all-properties", authmiddleware, propertycontroller.getallproperties)
+router.get("/get-all-properties", propertycontroller.getallproperties)
+router.get("/get-property/:id", propertycontroller.getoneproperty)
 
 module.exports = router
