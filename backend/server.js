4d1ad3b2ad684809be815fee253c3950cca0b743
imArpanGhimire <arpancalma@gmail.com>
2026-06-21 15:20:59 +0545

    Fix typo in server startup log message

--- backend/server.js
+++ backend/server.js
@@ -6,5 +6,5 @@ connectDB()
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(`server is running on pprt ${PORT}`)
+    console.log(`server is running on port ${PORT}`)
 })
