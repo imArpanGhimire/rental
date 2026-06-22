81a55f47a56fc1821ab692acdce40dc9dbdbee4a
imArpanGhimire <arpancalma@gmail.com>
2026-06-23 00:24:12 +0545

    Add review model with schema and unique index for property and reviewer

---
+++ backend/src/model/review.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+
+const reviewschema = new mongoose.Schema({
+    property: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "rental",
+        required: true
+    },
+    reviewer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    comment: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    ownerReply: {
+        comment: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        repliedAt: {
+            type: Date
+        }
+    }
+}, { timestamps: true }
+)
+
+reviewschema.index({ property: 1, reviewer: 1 }, { unique: true })
+
+const reviewmodel = mongoose.model("review", reviewschema)
+
+module.exports = reviewmodel
