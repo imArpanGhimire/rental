9c1b0713a33dcd3cceb34aea712b9c2082c3991e
imArpanGhimire <arpancalma@gmail.com>
2026-06-24 23:58:20 +0545

    wrote controller

--- backend/src/controller/review.controller.js
+++ backend/src/controller/review.controller.js
@@ -2,18 +2,17 @@ const rentalmodel = require("../model/rental.model")
 const reviewmodel = require("../model/review.model")
 
 async function createreview() {
-    // const {id}=req.query
-    const { rating, comment } = req.body
-    const propertyid = req.params.propertyid
-    //    const reviewerid=req.user._id
-
     try {
+        const { rating, comment } = req.user.body
+        const propertyid = req.params.propertyid
+        const reviewerid = req.user.id
+
+        const property = await rentalmodel.findById(propertyid)
+
+
 
     }
     catch (e) {
-        console.error(e)
-        return res.status(500).json({
-            message: "internal sever error"
-        })
+
     }
 }
