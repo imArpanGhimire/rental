6fc2508c736d9fcdf160446f6ba42a7c2f752cfe
imArpanGhimire <arpancalma@gmail.com>
2026-07-02 18:54:21 +0545

    feat: enhance edit reply functionality with error handling and response messages

--- backend/src/controller/review.controller.js
+++ backend/src/controller/review.controller.js
@@ -117,37 +117,46 @@ async function replytoreview(req, res) {
 }
 
 async function editreply(req, res) {
-    const reviewid = req.params.reviewid;
-    const review = await reviewmodel.findById(reviewid);
+    try {
+        const reviewid = req.params.reviewid;
+        const review = await reviewmodel.findById(reviewid);
 
-    const { comment } = req.body;
+        const { comment } = req.body;
 
-    if (!review) {
-        return res.status(404).json({
-            message: "couldn't found the reply",
-        });
+        if (!review) {
+            return res.status(404).json({
+                message: "couldn't found the reply",
+            });
+        }
+
+        const property = await rentalmodel.find(review.property);
+
+
+        // ownership check garya
+        if (property.owner.toString() !== req.user.id.toString()) {
+            return res.status(403).json({
+                message: "not authorised to edit",
+            });
+        }
+
+
+        if (!review.ownerReply || !review.ownerReply.comment) {
+            return res.status(400).json({ message: "No existing reply to edit" });
+        }
+
+
+        review.ownerReply.comment = comment
+        review.ownerReply.repliedAt = new Date()
+
+        await review.save()
+        res.status(200).json({ message: "Reply updated", review });
     }
-
-    const property = await rentalmodel.find(review.property);
-
-
-    // ownership check garya
-    if (property.owner.toString() !== req.user.id.toString()) {
-        return res.status(403).json({
-            message: "not authorised to edit",
-        });
+    catch (e) {
+        return res.status(500).json({
+            message: "internal server error",
+            error: e.message
+        })
     }
-
-
-    if (!review.ownerReply || !review.ownerReply.comment) {
-        return res.status(400).json({ message: "No existing reply to edit" });
-    }
-
-
-    review.ownerReply.comment = comment
-    review.ownerReply.repliedAt = new Date()
-
-    await review.save()
 }
 
 module.exports = {
