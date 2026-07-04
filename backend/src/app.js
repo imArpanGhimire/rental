d526174709d612f9b094554cea60fe10520bcd99
imArpanGhimire <arpancalma@gmail.com>
2026-07-05 00:05:33 +0545

    fix: correct formatting and spacing in app.js for improved readability

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,10 +8,10 @@ app.use(cookieParser())
 
 const authroutes = require("./routes/auth.routes")
 const propertyroutes = require("./routes/property.routes")
-
+const reviewroutes = require("./routes/review.routes")
 
 app.use("/api/auth", authroutes)
 app.use("/api/properties", propertyroutes)
-
+app.use("/api/reviews", reviewroutes)
 
 module.exports = app
