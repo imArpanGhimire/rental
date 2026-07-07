de9353403ae5dee0cb612e57303afc5ec0d88154
imArpanGhimire <arpancalma@gmail.com>
2026-07-07 15:24:07 +0545

    refactor: restructure savefavorite function for improved readability and error handling

--- backend/src/controller/favorites.controller.js
+++ backend/src/controller/favorites.controller.js
@@ -3,44 +3,55 @@ const rentalmodel = require("../model/rental.model")
 
 
 async function savefavorite(req, res) {
-    const propertyid = req.params.propertyid
-    const property = await rentalmodel.findById(id)
+    try {
+        const propertyid = req.params.propertyid
+        const property = await rentalmodel.findById(id)
 
-    const renterid = req.user.id
+        const renterid = req.user.id
 
-    if (!property) {
-        return res.status(404).json({
-            message: "Couldn't find that property"
+        if (!property) {
+            return res.status(404).json({
+                message: "Couldn't find that property"
+            })
+        }
+
+        if (req.user.role !== "renter") {
+            return res.status(403).json({
+                message: "Only renters can save properties",
+            });
+        }
+
+        // check if its already saved 
+        const alreadysaved = await favoritemodel.findOne({
+            renter: renterid,
+            property: propertyid
         })
-    }
 
-    if (req.user.role !== "renter") {
-        return res.status(403).json({
-            message: "Only renters can save properties",
+        if (alreadysaved) {
+            return res.status(400).json({
+                message: "Property already saved",
+            });
+        }
+
+        // save favorite
+        const favoriteproperty = await favoritemodel.create({
+            renter: renterid,
+            property: propertyid
+        })
+        return res.status(201).json({
+            message: "Property saved successfully",
+            favoriteproperty,
+        });
+
+    }
+    catch (e) {
+        console.error(e);
+
+        return res.status(500).json({
+            message: "Internal server error",
+            error: e.message,
         });
     }
-
-    // check if its already saved 
-    const alreadysaved = await favoritemodel.findOne({
-        renter: renterid,
-        property: propertyid
-    })
-
-    if (alreadysaved) {
-        return res.status(400).json({
-            message: "Property already saved",
-        });
-    }
-
-    // save favorite
-    const favoriteproperty = await favoritemodel.create({
-        renter: renterid,
-        property: propertyid
-    })
-    return res.status(201).json({
-        message: "Property saved successfully",
-        favoriteproperty,
-    });
 }
 
 module.exports = { savefavorite }
