aa96db5ff4be52f833dbf0c5e74c923733e09307
imArpanGhimire <arpancalma@gmail.com>
2026-07-08 20:44:49 +0545

    feat: update favorites routes to include property ID and authentication middleware

--- backend/src/routes/favorites.routes.js
+++ backend/src/routes/favorites.routes.js
@@ -2,9 +2,11 @@ const express = require("express")
 const router = express.Router()
 
 const favoritecontroller = require("../controller/favorites.controller")
+const authmiddleware = require("../middleware/auth.middleware")
 
-router.post("/add-forlater", favoritecontroller.saveForlater)
-router.post("/remove-forlater", favoritecontroller.removeForlater)
+router.post("/add-forlater/:propertyid", authmiddleware, favoritecontroller.saveForlater)
+router.post("/remove-forlater/:propertyid", authmiddleware, favoritecontroller.removeForlater)
+router.get("/get-forlater", authmiddleware, favoritecontroller.getForlater)
 
 
 
