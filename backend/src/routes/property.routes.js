149a43bd1fbe1eb65d347c9caca83d23eb461c60
imArpanGhimire <arpancalma@gmail.com>
2026-07-12 14:04:33 +0545

    fix: remove redundant middleware from view-my-listings route

--- backend/src/routes/property.routes.js
+++ backend/src/routes/property.routes.js
@@ -5,7 +5,6 @@ const router = express.Router()
 const authMiddleware = require("../middleware/auth.middleware")
 const ownershipMiddleware = require("../middleware/owner.middleware")
 const verifyPropertyMiddleware = require("../middleware/verifyproperty.middleware")
-const verifypropertyowner = require("../middleware/verifyproperty.middleware")
 
 // controllers
 const propertycontroller = require("../controller/property.controller")
@@ -19,6 +18,6 @@ router.get("/nearby", propertycontroller.getnearbyproperties)
 router.get("/get-property/:id", propertycontroller.getoneproperty)
 router.put("/update-property/:id", authMiddleware, ownershipMiddleware, verifyPropertyMiddleware, propertycontroller.updateproperty)
 router.delete("/delete-property/:id", authMiddleware, ownershipMiddleware, verifyPropertyMiddleware, propertycontroller.deleteproperty)
-router.get("/view-my-listings", authMiddleware, ownershipMiddleware, verifypropertyowner, propertycontroller.getmyproperties)
+router.get("/view-my-listings", authMiddleware, propertycontroller.getmyproperties)
 
 module.exports = router
