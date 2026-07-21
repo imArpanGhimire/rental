4c08d9e25f847bcf765d6e9725ad0d0ae55148d9
imArpanGhimire <arpancalma@gmail.com>
2026-07-21 23:25:21 +0545

    fix: remove unnecessary blank line in auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,7 +3,6 @@ const router = express.Router()
 
 const authcontroller = require("../controller/auth.controller")
 
-
 router.post("/register", authcontroller.registeruser)
 router.post("/login", authcontroller.loginuser)
 router.post("/logout", authcontroller.logoutuser)
