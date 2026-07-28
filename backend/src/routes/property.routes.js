3a5226447973bf950805850ae4a46d60b8d777cb
imArpanGhimire <arpancalma@gmail.com>
2026-07-28 23:45:37 +0545

    feat: integrate multer for image uploads in property routes

--- backend/src/routes/property.routes.js
+++ backend/src/routes/property.routes.js
@@ -1,23 +1,20 @@
 const express = require("express")
 const router = express.Router()
-
 // middlewares 
 const authMiddleware = require("../middleware/auth.middleware")
 const ownershipMiddleware = require("../middleware/owner.middleware")
 const verifyPropertyMiddleware = require("../middleware/verifyproperty.middleware")
-
+const verifypropertyowner = require("../middleware/verifyproperty.middleware")
+const upload = require("../middleware/upload.middleware")
 // controllers
 const propertycontroller = require("../controller/property.controller")
 
-
-router.post("/add-property", authMiddleware, ownershipMiddleware, propertycontroller.createproperty)
+router.post("/add-property", authMiddleware, ownershipMiddleware, upload.array("images", 5), propertycontroller.createproperty)
 router.get("/get-all-properties", propertycontroller.getallproperties)
-
 router.get("/nearby", propertycontroller.getnearbyproperties)
-
 router.get("/get-property/:id", propertycontroller.getoneproperty)
-router.put("/update-property/:id", authMiddleware, ownershipMiddleware, verifyPropertyMiddleware, propertycontroller.updateproperty)
+router.put("/update-property/:id", authMiddleware, ownershipMiddleware, verifyPropertyMiddleware, upload.array("images", 5), propertycontroller.updateproperty)
 router.delete("/delete-property/:id", authMiddleware, ownershipMiddleware, verifyPropertyMiddleware, propertycontroller.deleteproperty)
-router.get("/view-my-listings", authMiddleware, propertycontroller.getmyproperties)
+router.get("/view-my-listings", authMiddleware, ownershipMiddleware, verifypropertyowner, propertycontroller.getmyproperties)
 
 module.exports = router
