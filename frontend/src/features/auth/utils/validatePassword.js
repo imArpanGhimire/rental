67442e856221111f99bb99ec8460e902870bdc2d
imArpanGhimire <arpancalma@gmail.com>
2026-09-03 00:53:40 +0545

    feat: update password mismatch message for clarity

--- frontend/src/features/auth/utils/validatePassword.js
+++ frontend/src/features/auth/utils/validatePassword.js
@@ -1,6 +1,6 @@
 export function validateNewPassword(newPassword, confirmPassword) {
     if (newPassword !== confirmPassword) {
-        return "New passwords don't match.";
+        return "Make sure the new password are matching";
     }
     if (newPassword.length < 6) {
         return "New password must be at least 6 characters.";
