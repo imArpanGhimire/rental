dfed7de15a795904e9de833f112e6a62e6313fec
imArpanGhimire <arpancalma@gmail.com>
2026-09-03 00:44:51 +0545

    feat: add security questions for user authentication

---
+++ backend/src/config/securityQuestions.js
@@ -0,0 +1,10 @@
+const SECURITY_QUESTIONS = [
+    "What was the name of your first pet?",
+    "What is your mother's mother name?",
+    "What was the name of your first school?",
+    "What is your favorite movie?",
+    "What city were you born in?",
+    "What was your childhood nickname?",
+]
+
+module.exports = SECURITY_QUESTIONS
